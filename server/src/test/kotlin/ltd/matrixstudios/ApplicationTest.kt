d211b7057bf7789fdecc53522f4bbc7224d9a2b0
Max <67809373+98ping@users.noreply.github.com>
2024-02-19 19:37:02 -0800

    fix: fix tests

--- server/src/test/kotlin/ltd/matrixstudios/ApplicationTest.kt
+++ server/src/test/kotlin/ltd/matrixstudios/ApplicationTest.kt
@@ -4,14 +4,14 @@ import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.server.testing.*
-import ltd.matrixstudios.skeleton.main
+import ltd.matrixstudios.skeleton.SkeletonServer.Companion.main
 import kotlin.test.*
 
 class ApplicationTest {
     @Test
     fun testRoot() = testApplication {
         application {
-            main()
+            main(arrayOf())
         }
         client.get("/").apply {
             assertEquals(HttpStatusCode.OK, status)
