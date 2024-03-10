51d714ed96379e878c98109b613f9c530def0256
Max <67809373+98ping@users.noreply.github.com>
2024-03-09 20:27:15 -0800

    feat: help cmd

--- client/src/main.rs
+++ client/src/main.rs
@@ -5,6 +5,14 @@ use crate::r#mod::ImageHandler;
 #[tokio::main]
 async fn main() {
     println!("Skeleton Console");
+    println!(" ");
+    println!("Image Commands:");
+    for image_command in vec![
+        "image info <image_id> - View information about an image", "image launch <image_id> <template> <binded-port> <exposed-port> - Launch an image",
+    ] {
+        println!("- {image_command}");
+    }
+
     let mut image_handler = ImageHandler {
         sub_commands: vec![]
     };
